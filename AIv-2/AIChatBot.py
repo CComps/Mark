61df85fd15b9b7482f9a2191a3ec5c28341c5ccf
CComps <99871804+CComps@users.noreply.github.com>
2022-05-17 17:03:06 +0200

    __init__

---
+++ AIv-2/AIChatBot.py
@@ -0,0 +1,23 @@
+import json
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+lmt = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_words = ['?', '!', '.', ',', ':', ';', '-', '_', '*', '#', '@', '$', '%', '^', '&', '+', '=', '~', '`', '{', '}', '|', '\\', '<', '>', '"', "'", '(', ')', '[', ']', '\n', '\t', '\r']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        documents.append((w, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+
+print(documents)
