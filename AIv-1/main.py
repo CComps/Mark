bc291fb4eff31a5585647528ff86b6ad7b77e51d
CComps <99871804+CComps@users.noreply.github.com>
2022-05-18 20:09:40 +0200

    init

--- AIv-1/main.py
+++ AIv-1/main.py
@@ -24,6 +24,7 @@ def speak(audio):
     print("    ")
     print("-------------------")
     print("    ")
+
     if voice == "1":
         print(f"Friday: {audio}")
     else:
@@ -44,20 +45,26 @@ def weather():
     # now is complete url for weather like:
     # http://api.openweathermap.org/data/2.5/weather?appid=69e2f2cb7807397cb7f2d5cf6567fd30&q=Pezinok
     response = requests.get(complete_url)
+
     #     tell the user if the weather is clear, cloudy, rain, snow, sunny or thunderstorm
     x = response.json()
+
     if x["weather"][0]["main"] == "Clear":
         speak("The weather is clear")
         speak("Enjoy your day becuse it is sunny today")
+
     elif x["weather"][0]["main"] == "Clouds":
         speak("The weather is cloudy")
         speak("I know its not the best but it is ok")
+
     elif x["weather"][0]["main"] == "Rain":
         speak("The weather is rainy")
         speak("I know its very bad but it can be worse")
+
     elif x["weather"][0]["main"] == "Snow":
         speak("The weather is snowy")
         speak("Its very good day becuse you can go outside and enjoy the snow")
+
     elif x["weather"][0]["main"] == "Thunderstorm":
         speak("The weather is thunderstorm")
         speak("I don't kow what is worse than this")
@@ -66,6 +73,7 @@ def weather():
 
 
 # speak("Hallo Tom. I am your virtual assistant")
+
 
 if voice == "1":
     speak("initializing Friday")
@@ -95,54 +103,72 @@ def date():
 
 def wishMe():
     hour = datetime.datetime.now().hour
+
     if 6 <= hour < 12:
         speak("good morning boss")
         speak("How was your day look like?")
         query = takeCommand().lower()
+
         if "good" in query.lower():
             speak("Good to hear that")
+
         elif "great" in query.lower():
             speak("That's great to hear")
+
         elif "nice" in query.lower():
             speak("Very nice to hear")
+
         elif "bad" in query.lower():
             speak("I am sorry to hear that")
+
         elif "the worst" in query.lower():
             speak("I am sorry to hear that")
         else:
             pass
         speak(random.choice(["How can I help you with?", "What can I do for you?", "What can I do for you today?",
                              "how can I help you?"]))
+
     elif 12 <= hour < 18:
         speak("good afternoon boss")
         speak("How was your day look like?")
         query = takeCommand().lower()
+
         if "good" in query.lower():
             speak("Good to hear that")
+
         elif "great" in query.lower():
             speak("That's great to hear")
+
         elif "nice" in query.lower():
             speak("Very nice to hear")
+
         elif "bad" in query.lower():
             speak("I am sorry to hear that")
+
         elif "the worst" in query.lower():
             speak("I am sorry to hear that")
         else:
             pass
         speak(random.choice(["How can I help you with?", "What can I do for you?", "What can I do for you today?",
                              "how can I help you?"]))
+
     elif 18 <= hour < 22:
         speak("good evening boss")
         speak("How was your day look like?")
         query = takeCommand().lower()
+
         if "good" in query.lower():
             speak("Good to hear that")
+
         elif "great" in query.lower():
             speak("That's great to hear")
+
         elif "nice" in query.lower():
             speak("Very nice to hear")
+
         elif "bad" in query.lower():
             speak("I am sorry to hear that")
+
         elif "the worst" in query.lower():
             speak("I am sorry to hear that")
         else:
@@ -153,14 +179,19 @@ def wishMe():
         speak("good night boss")
         speak("How was your day look like?")
         query = takeCommand().lower()
+
         if "good" in query.lower():
             speak("Good to hear that")
+
         elif "great" in query.lower():
             speak("That's great to hear")
+
         elif "nice" in query.lower():
             speak("Very nice to hear")
+
         elif "bad" in query.lower():
             speak("I am sorry to hear that")
+
         elif "the worst" in query.lower():
             speak("I am sorry to hear that")
         else:
@@ -173,6 +204,7 @@ def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         audio = r.listen(source, 0, 3)
+
     try:
         query = r.recognize_google(audio, language='en-in')  # en-in
         print(f"you: {query}\n")  # User query will be printed.
@@ -185,19 +217,23 @@ def takeCommand():
 def mark():
     while True:
         query = takeCommand().lower()
+
         if "wake up" in query:
             wishMe()
             while 1:
                 query = takeCommand().lower()
+
                 if "open youtube" in query.lower():
                     webbrowser.open("https://youtube.com")
                     speak("OK. I opened YouTube")
+
                 elif "wikipedia" in query.lower():
                     speak("searching wikipedia")
                     query = query.replace("wikipedia", "")
                     results = wikipedia.summary(query, sentences=2)
                     speak("According to wikipedia")
                     speak(results)
+
                 elif "play don't stop" in query.lower():
                     webbrowser.open("https://www.youtube.com/watch?v=_SGBcmdWgGY&ab_channel=Cubixlol")
                     pyautogui.press('f5')
@@ -206,28 +242,37 @@ def mark():
                     time.sleep(5)
                     pyautogui.press('t')
                     pyautogui.press('f')
+
                 elif "for nothing" in query.lower():
                     speak("I am sorry. But your rude!")
+
                 elif "web" in query.lower():
                     webbrowser.open("http://192.168.1.38/")
                     speak("OK. I opened our website")
+
                 elif "open google" in query.lower():
                     webbrowser.open("http://www.google.com")
                     speak("OK. I opened Google")
+
                 elif "what's the time" in query.lower():
                     speak("It's:     ")
                     Time()
+
                 elif "what's the date" in query.lower():
                     speak("The date is: ")
                     date()
+
                 elif "open stack overflow" in query.lower():
                     webbrowser.open("https://stackoverflow.com")
                     speak("OK. I opened Stack Overflow")
+
                 elif "open programming app" in query.lower():
                     webbrowser.open("https://github.com")
                     speak("OK. I opened GitHub")
+
                 elif "how are you" in query.lower():
                     speak("I am fine. Thanks for asking")
+
                 elif "play" in query.lower() and "remix" in query.lower():
                     speak("OK. I am playing your favorite remix")
                     webbrowser.open("https://www.youtube.com/watch?v=m2xRXef1MOE&ab_channel=MichaelMusician")
@@ -235,26 +280,34 @@ def mark():
                     time.sleep(5)
                     pyautogui.press('t')
                     pyautogui.press('f')
+
                 elif "clear console" in query.lower():
                     speak("Clearing console")
                     os.system('cls')
+
                 elif "clear training" in query.lower():
                     speak("Clearing training")
                     with open("training.txt", "w") as f:
                         f.write(",")
                     speak("Training was cleared")
+
                 elif "search" in query.lower():
                     query = query.replace("search", "")
                     pywhatkit.search(query)
+
                 elif "who are you" in query.lower():
                     speak("I am your personal assistant. My name is mark")
+
                 elif "who made you" in query.lower():
                     speak("I was made by Tom")
+
                 elif "who is your creator" in query.lower() or "who's your creator" in query.lower():
                     speak("I was made by Tom")
+
                 elif "open whatsapp" in query.lower():
                     webbrowser.open("https://web.whatsapp.com")
                     speak("OK. I opened Whatsapp")
+
                 elif "play" in query.lower():
                     song = query.replace("play", "")
                     pywhatkit.playonyt(song)
@@ -263,75 +316,98 @@ def mark():
                     time.sleep(8)
                     pyautogui.press('t')
                     pyautogui.press('f')
+
                 elif "window to left" in query.lower():
                     speak("The window is switched to left")
                     pyautogui.hotkey('winleft', 'left')
+
                 elif "window to right" in query.lower():
                     speak("The window is switched to right")
                     pyautogui.hotkey('winleft', 'right')
+
                 elif "window to up" in query.lower():
                     speak("The window is switched to up")
                     pyautogui.hotkey('winleft', 'up')
+
                 elif "window to down" in query.lower():
                     speak("The window is switched to down")
                     pyautogui.hotkey('winleft', 'down')
+
                 elif "close window" in query.lower():
                     speak("Closing window")
                     pyautogui.hotkey('altleft', 'f4')
+
                 elif "close this window" in query.lower():
                     speak("Closing window")
                     pyautogui.hotkey("altleft", 'f4')
+
                 elif "window" in query.lower():
                     speak("Switching window")
                     pyautogui.hotkey('altleft', 'tab')
+
                 elif "tab" in query.lower():
                     speak("Switching tab")
                     pyautogui.hotkey('ctrlleft', 'tab')
+
                 elif "minimize" in query.lower():
                     speak("Minimizing window")
                     pyautogui.hotkey('winleft', 'm')
+
                 elif "maximize" in query.lower():
                     speak("Maximizing window")
                     pyautogui.hotkey('winleft', 'up')
+
                 elif "close this" in query.lower():
                     speak("Closing tab")
                     pyautogui.hotkey('ctrlleft', 'w')
+
                 elif "open wikipedia" in query.lower():
                     webbrowser.open("https://wikipedia.org")
                     speak("OK. I opened Wikipedia")
+
                 elif "open programming studio" in query.lower():
                     os.startfile(
                         "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\JetBrains\\"
                         "PyCharm Community Edition 2022.1.lnk")
                     speak("OK. I opened your Programming Studio named PyCharm")
+
                 elif "how" in query.lower() and "day" in query.lower() and "you" in query.lower():
                     speak("My day was good")
+
                 elif "search" in query.lower():
                     speak("What do you want to search?")
                     search = takeCommand().lower()
                     a = "Here is what I found for " + search
                     webbrowser.open("https://www.google.com/search?q=" + search)
                     speak(a)
+
                 elif "what's your version" in query.lower() or "what is your version" in query.lower():
                     speak("I am version A.2")
                     print("(A.1 now does not exist)")
+
                 elif "what is your name" in query.lower() or "what's your name" in query.lower():
+
                     if voice == 0:
                         speak("My name is Mark")
                     else:
                         speak("My name is Friday")
+
                 elif "how old are you" in query.lower():
                     speak("I am 3 months old")
+
                 elif query == "mark" or query == "friday":
                     speak("Yes?")
+
                 elif "say hello" in query.lower():
                     speak("Hello.")
                     with open("voice.txt", "r") as f:
                         voice = int(f.read())
+
                     if voice == 0:
                         speak("I am Mark")
                     else:
                         speak("I am Friday")
+
                 elif "change to friday" in query.lower():
                     speak("I am changing to Friday")
                     with open("voice.txt", "w") as f:
@@ -339,9 +415,11 @@ def mark():
                     with open("voice.txt", "r") as f:
                         voice = int(f.read())
                     engine.setProperty('voice', voices[voice].id)
+
                 elif "you" in query.lower() and "sure" in query.lower():
                     speak("Yes")
                     speak("I am Always sure")
+
                 elif "change to mark" in query.lower() or "change to marc" in query.lower():
                     speak("I am changing to Mark")
                     with open("voice.txt", "w") as f:
@@ -349,42 +427,54 @@ def mark():
                     with open("voice.txt", "r") as f:
                         voice = int(f.read())
                     engine.setProperty('voice', voices[voice].id)
+
                 elif "weather" in query.lower():
                     weather()
+
                 elif "tell" in query.lower() and "about you" in query.lower():
                     speak("Let me introduce myself")
                     speak("I am a computer program!")
                     speak("I was made by Tom")
                     speak("I was created on December 2021")
                     speak("I am version A.2")
+
                     speak("if you want to know what can I do for you, just ask 'what can you do?'")
+
                 elif "intro" in query.lower():
                     os.startfile("C:\\Users\\Mark\\Desktop\\Intro.mp4")
                     speak("Hello, I am Friday. Your personal assistant. I am here to help you.")
                     speak("I can open websites, search for information, play music, pause music, and play videos.")
                     speak("I can also change my voice to Mark or Friday.")
                     speak("If you want to know more about me, just ask 'tell me about yourself'.")
+
                 elif "thank" in query.lower():
                     speak(random.choice(
                         ["You're welcome", "No problem", "My pleasure", "It was my pleasure to help you"]))
+
                 elif "on the light" in query.lower():
                     speak("turning on the light")
+
                 elif "off the light" in query.lower():
                     speak("turning off the light")
+
                 elif "lock the door" in query.lower():
                     speak("locking the door")
+
                 elif "unlock the door" in query.lower():
                     speak("unlocking the door")
+
                 elif 'exit' in query.lower() or 'quit' in query.lower() or 'goodbye' in query.lower() or \
                         'sleep' in query.lower():
                     speak("Bye bye. Have a nice day.")
                     break
+
                 elif "what can you do" in query.lower():
                     speak("I can search Wikipedia, open YouTube, play music, open Google, open Stack Overflow, "
                           "open GitHub(programming app), open Whatsapp, tell you time, tell you date, control your "
                           "video. And that's it!")
                 else:
                     pass
+
         elif "bye" in query.lower():
             break
         else:
@@ -393,6 +483,7 @@ def mark():
 
 # wishMe()
 # query = takeCommand()
+
 
 if __name__ == "__main__":
     speak("Initializing is Done!")
